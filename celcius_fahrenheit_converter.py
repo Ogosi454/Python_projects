a34acfd95ab426fe766f5d77db7a8bd457a4432e
Harizon Obura <harizonoburaakech@gmail.com>
2025-11-18 07:06:39 +0300

    prgram converting celcius to fahrenheit

---
+++ celcius_fahrenheit_converter.py
@@ -0,0 +1,11 @@
+def coverter(fahrenheit):
+    return fahrenheit = (9/5 * C) + 32
+
+
+def main():
+    C = int(input("Enter temperature in degrees celcius: "))
+
+    return fahrenheit
+
+if __name__ == "__main__":
+    main()
