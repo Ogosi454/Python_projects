457dd35a2882ec922b394865959d9f0b2146d32a
Harizon Obura <harizonoburaakech@gmail.com>
2025-11-29 11:53:25 +0300

    This progra print the fibonacci sequce upto the nth term

---
+++ fibonacci_sequence.py
@@ -0,0 +1,19 @@
+# This program prints the fibonacci sequence upto nth term
+#
+n = int(input("Enter the number of terms in Fibonacci sequence: "))
+a, b = 0, 1
+count = 0
+if n <= 0:
+    print("Please enter a positive integer.")
+elif n == 1:
+    print("Fibonacci sequence upto", n, ":")
+    print(a)
+else:
+    print("Fibonacci sequence:")
+    while count < n:
+        print(a)
+        nth = a + b
+        a = b
+        b = nth
+        count += 1
+
